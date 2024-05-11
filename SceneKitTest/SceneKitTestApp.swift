a9d87ebaedefdb17e4e69b96fed594304e735760
Dave-Ed-Cast <dcastaldi23@fed.idserve.net>
2024-05-11 15:51:34 +0200

    initial commit

---
+++ SceneKitTest/SceneKitTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SceneKitTestApp.swift
+//  SceneKitTest
+//
+//  Created by Davide Castaldi on 11/05/24.
+//
+
+import SwiftUI
+
+@main
+struct SceneKitTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
